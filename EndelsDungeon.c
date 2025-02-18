b198744c492d792d5b6620b7547ca8567d4b7a6b
Zätha Malmberg <zathamalmberg@gmail.com>
2025-02-18 12:36:24 +0100

    started woking in room and coridors

--- EndelsDungeon.c
+++ EndelsDungeon.c
@@ -2,13 +2,29 @@
 
 int main(void)
 {
-    InitWindow(800, 450, "raylib [core] example - basic window");
+    InitWindow(1000, 600, "Endless Dugneon");
+    
+    Rectangle Room[3] = {{400, 200, 100, 100}, 
+    {400, 200, 100, 100}, 
+    {400, 200, 100, 100}};
+    
+    Rectangle Coridor[2] = {{600, 300, 100, 100}, {600, 300, 100, 100}};
+    
+    
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            
+            for(int i = 0; i < 3; i++){
+                DrawRectangle(Room[i].x, Room[i].y, Room[i].width, Room[i].height, BLACK);
+            }
+            for(int i = 0; i < 2; i++){
+                DrawRectangle(Coridor[i].x, Coridor[i].y, Coridor[i].width, Coridor[i].height, BLACK);
+            }
+            
+            //DrawRectangle(Room.x, Room.y, Room.width, Room.height, BLACK);
         EndDrawing();
     }
 
